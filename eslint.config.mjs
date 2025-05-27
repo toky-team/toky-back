25d94d887f70d63b43566c4a7bcb29cdbe1918fd
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-05-27 20:52:54 +0900

    chore: eslint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,27 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import moduleResolver from 'eslint-plugin-module-resolver';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config(
   {
     ignores: ['eslint.config.mjs'],
   },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
   eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommendedTypeChecked,
   {
+    files: ['**/*.ts'],
     languageOptions: {
       globals: {
         ...globals.node,
         ...globals.jest,
       },
-      ecmaVersion: 5,
+      ecmaVersion: 2020,
       sourceType: 'module',
       parserOptions: {
         projectService: true,
@@ -26,10 +30,59 @@ export default tseslint.config(
     },
   },
   {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+    files: ['**/*.ts'],
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      prettier: eslintPluginPrettier,
+      'module-resolver': moduleResolver,
     },
-  },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+
+      'module-resolver/use-alias': [
+        'error',
+        {
+          alias: {
+            '~': './src',
+          },
+        },
+      ],
+      'no-restricted-imports': [
+        'error',
+        {
+          patterns: [
+            {
+              group: ["./**", "../**"],
+              message: 'Please use absolute imports instead of relative ones.',
+            }
+          ]
+        }
+      ],
+
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      'no-var': 'warn',
+      'prefer-const': 'warn',
+      'eqeqeq': 'warn',
+      'prefer-arrow-callback': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+      }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
+
+    },
+  }
 );
