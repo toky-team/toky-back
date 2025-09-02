4d47ffad28b282868eff985d5cdab3520ccaa213
Kim SeongHyeon <kimsh4265@korea.ac.kr>
2025-09-02 13:08:47 +0900

    feat: 게임 상태 반환

--- src/modules/attendance/presentation/http/dto/attendance.response.dto.ts
+++ src/modules/attendance/presentation/http/dto/attendance.response.dto.ts
@@ -2,6 +2,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 import { DateUtil } from '~/libs/utils/date.util';
 import { AttendancePrimitives } from '~/modules/attendance/domain/model/attendance';
+import { AttendanceGameStatus } from '~/modules/attendance/domain/model/game-status.vo';
 
 export class AttendanceResponseDto {
   @ApiProperty({
@@ -15,6 +16,13 @@ export class AttendanceResponseDto {
     example: true,
   })
   isAttended: boolean;
+
+  @ApiProperty({
+    description:
+      "게임 상태, 출석체크를 시작하기 전이면 '시작 전', 게임을 하고 있으면 '진행 중', 출석체크가 끝났으면 '종료', 공유를 통해 기회를 얻은 경우 다시 '진행 중'으로 돌아갑니다.",
+    enum: AttendanceGameStatus,
+  })
+  gameStatus: AttendanceGameStatus;
 
   @ApiProperty({
     description: '1단계 게임 클리어 여부(null 이면 아직 게임 완료 X)',
@@ -36,6 +44,7 @@ export class AttendanceResponseDto {
     const dto = new AttendanceResponseDto();
     dto.attendandAt = DateUtil.format(primitives.attendandAt);
     dto.isAttended = primitives.isAttended;
+    dto.gameStatus = primitives.gameStatus;
     dto.firstStageResult = primitives.firstStageResult;
     dto.secondStageResult = primitives.secondStageResult;
     return dto;
